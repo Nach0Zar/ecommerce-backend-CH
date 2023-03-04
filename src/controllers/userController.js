0f0ea4a4e51956796a839663a792f56092ebc991
Nach0Zar <50642766+Nach0Zar@users.noreply.github.com>
2023-03-04 18:02:56 -0300

    Removed usersContainer and fixed path errors

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,29 @@
-import userContainerDB from '../models/containers/usersContainer.js';
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import config from '../config/config.js';
 import jwt from 'jsonwebtoken';
-import { mailer } from '../models/models/mailer.js';
+import { mailer } from '../models/mailer.js';
 import cartController from './cartController.js';
 import request from 'request';
+import { PERSISTENCIA } from '../db/config/config.js';
+import FirestoreContainer from '../containers/firestoreContainer.js';
+import MongoDBContainer from '../containers/mongoDBContainer.js';
+import MemoryFSContainer from '../containers/MemoryFSContainer.js';
 
 class userControllerClass{
-    constructor(userContainer){
-        this.userContainer = userContainer;
+    #container
+    constructor(){
+        switch (PERSISTENCIA) {
+            case 'mongodb': 
+                this.#container = new MongoDBContainer("products")
+                break
+            case 'firebase':
+                this.#container = new FirestoreContainer("products")
+                break
+            default:
+                this.#container = new MemoryFSContainer("products")
+                break
+        }
     }
     serializeUserMongo = (user, done) => {
         done(null, user.id);
@@ -29,7 +43,7 @@ class userControllerClass{
                     password: jwt.sign(req.body.password1, config.SESSION.secret),
                     cart: cartID
                 }
-                this.userContainer.getItemByEmail(user.email).then((userFound)=>{
+                this.userContainer.getItemByCriteria({email: user.email}).then((userFound)=>{
                     if(userFound === null){
                         this.userContainer.save(user).then(()=>{
                             mailer.send({
@@ -48,44 +62,13 @@ class userControllerClass{
                 console.log(err)
                 res.sendStatus(500)
             })
-            // request.post(`http://localhost:${config.PORT}/api/shoppingcart`, (err, response, body) => {
-            //     if (err) { 
-            //         res.sendStatus(500)
-            //     }
-            //     else{
-            //         let cartID = JSON.parse(response.body).id
-            //         const user = {
-            //             firstname: req.body.firstname,
-            //             lastname: req.body.lastname,
-            //             email: req.body.username,
-            //             profileImage: "blank",
-            //             password: jwt.sign(req.body.password1, config.SESSION.secret),
-            //             cart: cartID
-            //         }
-            //         this.userContainer.getItemByEmail(user.email).then((userFound)=>{
-            //             if(userFound === null){
-            //                 this.userContainer.save(user).then(()=>{
-            //                     mailer.send({
-            //                         to: config.MAIL_ADMIN,
-            //                         subject: 'nuevo registro!',
-            //                         text: `nuevo registro: ${JSON.stringify(user)}`
-            //                     })
-            //                     res.sendStatus(201)
-            //                 })
-            //             }
-            //             else{
-            //                 res.sendStatus(409)
-            //             }
-            //         })
-            //     }
-            // });
         }
         else{
             res.sendStatus(500)
         }
     }
     loginUser = (req, res) => {
-        this.userContainer.getItemByEmail(req.body.username).then((item)=>{
+        this.userContainer.getItemByCriteria({email: req.body.username}).then((item)=>{
             if(item){
                 res.cookie('email', item.email, {maxAge: config.SESSION.EXPIRY_TIME})
                 res.sendStatus(200)
@@ -100,7 +83,7 @@ class userControllerClass{
         res.sendStatus(200);
     }
     controllerGetUserInformation = (req, res) => {
-        this.userContainer.getItemByEmail(req.cookies.email).then((item)=>{
+        this.userContainer.getItemByCriteria({email: req.cookies.email}).then((item)=>{
             if(item){
                 res.json(item)
                 res.status(200)
@@ -111,7 +94,7 @@ class userControllerClass{
         })
     }
     controllerGetUserCartInformation = (req, res) => {
-        this.userContainer.getItemByEmail(req.cookies.email).then((item)=>{
+        this.userContainer.getItemByCriteria({email: req.cookies.email}).then((item)=>{
             if(item){
                 request.get(`http://localhost:${config.PORT}/api/shoppingcart/${item.cart}/products`, (err, response, body) => {
                     if (err) { 
@@ -130,12 +113,12 @@ class userControllerClass{
     }
 }
 
-const userController = new userControllerClass(new userContainerDB())
+const userController = new userControllerClass(new MongoDBContainer("users"))
 Object.freeze(userController);
 passport.use('local-login', new LocalStrategy(
     {},
     (username, password, done) => {
-        userController.userContainer.getItemByEmail(username).then((user)=>{
+        userController.userContainer.getItemByCriteria({email: username}).then((user)=>{
             if(user){
                 const originalPassword = jwt.verify(user.password, config.SESSION.secret)
                 if (password !== originalPassword) {
