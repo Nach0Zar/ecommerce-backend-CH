588bd2565d8bad7197a1a1bd034801922aff86c5
Nach0Zar <50642766+Nach0Zar@users.noreply.github.com>
2023-03-05 19:17:57 -0300

    Added logger

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,9 +1,11 @@
 import cartService from '../services/cartService.js';
+import logger from '../utils/logger.js';
 
 class CartControllerClass{
     controllerGetCartProducts = async (req, res, next) => {
         try{
             let products = await cartService.getCartProducts(req.params.id_cart);
+            logger.info(`GET REQUEST successfull for cart products ID ${req.params.id_cart}`);
             res.status(200).json(products);
         }
         catch(error){
@@ -13,6 +15,7 @@ class CartControllerClass{
     controllerPostCart = async (req, res, next) => {
         try{
             let newID = await cartService.createCart(req.body.products);
+            logger.info(`POST REQUEST successfull for cart ID ${newID}`);
             res.status(201).json({id: newID});
         }
         catch(error){
@@ -22,6 +25,7 @@ class CartControllerClass{
     controllerPostProductToCart = async (req, res, next) => {
         try{
             let cartProducts = await cartService.addProductToCart(req.params.id_cart, req.body.id_prod);
+            logger.info(`POST REQUEST successfull for product ${req.body.id_prod} in cart ${req.params.id_cart}`);
             res.status(200).json(cartProducts);
         }
         catch(error){
@@ -31,6 +35,7 @@ class CartControllerClass{
     controllerDeleteAllProductsFromCart = async (req, res, next) => {
         try{
             await cartService.deleteAllProductsFromCart(req.params.id_cart);
+            logger.info(`DELETE REQUEST successfull for products in cart ID ${req.params.id_cart}`);
             res.status(200).json({message: `el carrito ${req.params.id_cart} fue vaciado.`})
         }
         catch(error){
@@ -40,8 +45,8 @@ class CartControllerClass{
     controllerDeleteProductFromCartByID = async (req, res, next) => {
         try{
             await cartService.deleteProductFromCartByID(req.params.id_cart, req.params.id_prod);
+            logger.info(`DELETE REQUEST successfull for product ${req.params.id_prod} in cart ID ${req.params.id_cart}`);
             res.status(200).json({message: `el item ${req.params.id_prod} del carrito ${req.params.id_cart} fue eliminado.`}) 
-        
         }
         catch(error){
             next(error)
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,14 @@
 import config from '../config/config.js';
 import userService from '../services/userService.js';
 import registerUserValidation from '../validations/registerUserValidation.js';
+import logger from '../utils/logger.js';
 
 class UserControllerClass{
     controllerPostRegisterUser = async (req, res, next) => {
         try{
             await registerUserValidation(req);
             let userID = await userService.registerUser(req.body);
+            logger.info(`POST REQUEST successfull for registering user ${userID}`);
             res.status(201).json(userID)
         }
         catch(error){
@@ -16,6 +18,7 @@ class UserControllerClass{
     controllerPostLogInUser = async (req, res, next) => {
         try{
             let userEmailValidated = await userService.loginUser(req.body.username);
+            logger.info(`POST REQUEST successfull for logging in user with email ${userEmailValidated}`);
             res.cookie('email', userEmailValidated, {maxAge: config.SESSION.EXPIRY_TIME});
             res.sendStatus(200);
         }
@@ -25,11 +28,13 @@ class UserControllerClass{
     }
     controllerPostLogOutUser = (req, res) => {
         res.clearCookie('email');
+        logger.info(`POST REQUEST successfull for logging out user`);
         res.sendStatus(200);
     }
     controllerGetUserInformation = async (req, res, next) => {
         try{
             let userInformation = await userService.getUserInformation(req.cookies.email);
+            logger.info(`GET REQUEST successfull for getting the information of user ${req.cookies.email}`);
             res.status(200).json(userInformation);
         }
         catch(error){
@@ -39,6 +44,7 @@ class UserControllerClass{
     controllerGetUserCartInformation = async (req, res, next) => {
         try{
             let cartInformation = await userService.getUserCartInformation(req.cookies.email);
+            logger.info(`GET REQUEST successfull for getting cart information from user ${req.cookies.email}`);
             res.status(200).json(cartInformation);
         }
         catch(error){
